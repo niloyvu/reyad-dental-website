1759d8890cc31330b8ee95f8b1bdcb1b18a4def5
niloykb <niloybarmonvu@gmail.com>
2024-02-21 13:56:03 +0600

    scroll to top and unused import has been removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,18 @@
-import { filter } from 'rxjs';
-import { HttpClientModule } from '@angular/common/http';
-import { HomeComponent } from './pages/home/home.component';
-import { ShopComponent } from './pages/shop/shop.component';
-import { BlogsComponent } from './pages/blogs/blogs.component';
-import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
-import { Component, HostListener, OnInit, inject } from '@angular/core';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
-import { AboutUsComponent } from './pages/about-us/about-us.component';
-import { ServicesComponent } from './pages/services/services.component';
-import { ContactUsComponent } from './pages/contact-us/contact-us.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    ShopComponent,
-    BlogsComponent,
-    HomeComponent,
     FooterComponent,
     HeaderComponent,
-    HttpClientModule,
-    AboutUsComponent,
-    ServicesComponent,
-    ContactUsComponent,
   ],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
 
-  buttonOpacity: number = 0;
-  router = inject(Router)
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    if (scrollPosition > 200) {
-      this.buttonOpacity = 1;
-    } else {
-      this.buttonOpacity = 0;
-    }
-  }
-
-  ngOnInit(): void {
-    this.router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe(() => {
-        this.scrollToTop();
-      });
-  }
-
-  scrollToTop() {
-    if (typeof window !== "undefined") {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
-    }
-  }
-
-}
+export class AppComponent {}
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,26 @@
 import { routes } from './app.routes';
 import { provideToastr } from 'ngx-toastr';
-import { provideRouter, withViewTransitions } from '@angular/router';
 import { ApplicationConfig } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { loggingInterceptor } from './interceptors/logging.interceptor';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { InMemoryScrollingFeature, InMemoryScrollingOptions, provideRouter, withInMemoryScrolling, withViewTransitions } from '@angular/router';
+
+const scrollConfig: InMemoryScrollingOptions = {
+  scrollPositionRestoration: 'top',
+  anchorScrolling: 'enabled',
+};
+
+const inMemoryScrollingFeature: InMemoryScrollingFeature = withInMemoryScrolling(scrollConfig);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideToastr(),
     provideAnimations(),
     provideRouter(routes,
-      withViewTransitions()
+      withViewTransitions(),
+      inMemoryScrollingFeature
     ),
     provideClientHydration(),
     provideHttpClient(
